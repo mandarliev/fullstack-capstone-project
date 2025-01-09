7db1b0ab0715542345db2c8dcaa7030438582670
mandarliev <dimitar.mandarliev@gmail.com>
2025-01-09 02:16:33 -0500

    Implemented Back-end in Profile Component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,22 +4,25 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
-  return (
+    const navigate = useNavigate();
+    return (
         <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/search" element={<SearchPage/>} />
-        </Routes>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
+
+                <Route path="/app/profile" element={<Profile />} />
+            </Routes>
         </>
-  );
+    );
 }
 export default App;
